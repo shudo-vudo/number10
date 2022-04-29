261561689d3287719a1e5bd411833208ae4da4bc
klimas <dj121jd@gmail.com>
2022-04-29 17:48:08 +0300

    Добавьте файлы проекта.

---
+++ number10/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "human.h"
+
+int main()
+{
+    Student klim("Shuvaev", "Klim", "Sergeevich", 19, true);
+    Boss karpov("Karpov", "Dmitrii", "Anatolievich", 46, 28);
+    klim.print();
+    cout << endl;
+    karpov.print();
+}
